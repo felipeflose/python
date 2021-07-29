51d5b45cf8fb4368818a10c9e9e7651d8bce0732
felipeflose <44748710+felipeflose@users.noreply.github.com>
2021-07-29 20:05:52 -0300

    Add files via upload

---
+++ strings.py
@@ -0,0 +1,28 @@
+print ("Ola")
+print ('Ola')
+
+w = "teste"
+print(w)
+
+
+a = """olá
+resre
+tetete"""
+
+print(a)
+
+
+b = " ola mundo     "
+print(b.strip())
+
+c = "OLA MUNDO"
+print(c.lower())
+
+
+c = "OLA MUNDO"
+print(c.upper())
+
+e = "ola"
+f = "mundo"
+g = e+" " +f
+print(g)
---
+++ testeInternet1.py
@@ -0,0 +1,27 @@
+from speedtest import Speedtest
+from datetime import datetime
+import pandas as pd
+import time
+import matplotlib.pyplot
+
+st = Speedtest()
+mb = 10**-6
+dt = datetime.today()
+d = st.download()*mb
+u = st.upload()*mb
+#print(f"Velocidade de Download: {st.download()*mb} MB em {dt.strftime('%d/%m/%Y %H:%M')}  ") 
+#print(f"Velocidade de Upload: {st.upload()*mb} MB em {dt.strftime('%d/%m/%Y %H:%M')}" )
+i = 0
+
+
+while i < 200:
+    
+    dt = datetime.today()
+    d = st.download()*mb
+    u = st.upload()*mb  
+    df = pd.DataFrame({'data':dt, 'id':i, 'download':d,'upload':u},index=[i])
+    i += 1
+    time.sleep(1)
+    #print(df)
+
+    matplotlib.pyplot.plot(df.data , df.d)

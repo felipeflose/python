916d843f667bebd13edc70814e86e7459f7cdb0d
Felipe <feelip.flose@gmail.com>
2021-08-02 22:19:26 +0000

    aula dia 02.08

---
+++ boolean.py
@@ -0,0 +1,31 @@
+# no python semopre usamos True e False
+
+#operadores lógicos
+
+#and
+#or 
+#not
+
+Criando uma tabela verdade
+
+#AND
+
+# True and True = True
+# True and False = False
+# False and True = False
+# False and False = False
+
+#OR
+
+# True or True = True
+# True or False = True
+# False or True = True
+# False or False = False
+
+
+#NOT
+
+# True not True = True
+# True not False = True
+# False not True = True
+# False not False = False
---
+++ input.py
@@ -0,0 +1,6 @@
+#funcao input
+
+print("Qual é o seu nome")
+nome = input()
+print("Olá, "+nome)
+#print("Olá, ",nome) pode ser usar virgula

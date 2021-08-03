476b7716fd5437eae24e0fb682a4cc2222df5fcd
Felipe <feelip.flose@gmail.com>
2021-08-03 01:55:03 +0000

    ikjoij

--- atualizaGit.py
+++ atualizaGit.py
@@ -1,10 +1,8 @@
 
-import os
-
+import git 
 caminho = input("Entre com o caminho para Atualizacao: ")
-#comandoGitPull = "git pull"
-#comandoFinder = "cd /"
-#comandoFinderLocaliza = comandoFinder+caminho
-
-os.chdir("cd ")
-
+comandoGitPull = "git pull"
+comandoFinder = "cd /"
+comandoFinderLocaliza = comandoFinder+caminho
+g = git.cmd.Git(comandoFinderLocaliza)
+g.pull()

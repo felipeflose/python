c3bd451a73fc84af3a7b79e27113411c1f39ba1b
Felipe <feelip.flose@gmail.com>
2021-08-03 01:37:50 +0000

    csdsac

--- atualizaGit.py
+++ atualizaGit.py
@@ -2,8 +2,9 @@
 import os
 
 caminho = input("Entre com o caminho para Atualizacao: ")
-comandoGitPull = "git pull"
-comandoFinder = "cd /"
-comandoFinderLocaliza = comandoFinder+caminho
+#comandoGitPull = "git pull"
+#comandoFinder = "cd /"
+#comandoFinderLocaliza = comandoFinder+caminho
 
 os.chdir("cd ")
+

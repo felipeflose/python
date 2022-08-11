71a1d033664113520f0b75462f7e8f7c46773389
Felipe <feelip.flose@gmail.com>
2022-08-11 12:07:38 -0300

    sasas

--- Producao/ex_py_internet_v3.py
+++ Producao/ex_py_internet_v3.py
@@ -1,132 +1,118 @@
+
+from lib2to3.pgen2.token import NEWLINE
 import os
 import csv
+from re import X
 import time
+import turtle
 import speedtest
 from datetime import datetime
 from time import sleep
 import uuid
+import numpy as np
+import pandas as pd
+import os
+import csv
+import time
+import glob
+import sqlite3
 
-def teste_internet():   
-    # Instanciando a função de test do Speedtest
-    s = speedtest.Speedtest()
-    s.get_servers()
-    s.get_best_server()
+
+class internet:
+
+    def __init__(self, teste):
+        self.teste = teste
+
+    def teste_internet():
+        # Instanciando a função de test do Speedtest
+        s = speedtest.Speedtest()
+        s.get_servers()
+        s.get_best_server()
     # Testando velocidades
-    velocidade_download = round(s.download(threads=None)*(10**-6))
-    velocidade_upload = round(s.upload(threads=None)*(10**-6))
+        velocidade_download = round(s.download(threads=None)*(10**-6))
+        velocidade_upload = round(s.upload(threads=None)*(10**-6))
     # Capturando data e hora do teste
-    data_atual = datetime.now().strftime('%d/%m/%Y')
-    hora_atual = datetime.now().strftime('%H:%M:%S')   
-    
-    return data_atual, hora_atual, velocidade_download, velocidade_upload
-   
-def cria_lista():       
+        data_atual = datetime.now().strftime('%d/%m/%Y')
+        hora_atual = datetime.now().strftime('%H:%M:%S')
+        return data_atual, hora_atual, velocidade_download, velocidade_upload
+
+    def cria_item():
         lista = []
-        try:        
-                data_atual, hora_atual, velocidade_download, velocidade_upload = teste_internet()
-                lista.append(uuid.uuid1())
+        while len(lista) == 0:
+            try:
+                data_atual, hora_atual, velocidade_download, velocidade_upload = internet.teste_internet()
+                #id = uuid.uuid1()
+                # lista.append(id)
                 lista.append(data_atual)
                 lista.append(hora_atual)
-                lista.append(datetime.now().strftime('%H')) 
                 lista.append(velocidade_download)
-                if velocidade_download >= 200:
-                        lista.append(1)
-                else:   lista.append(0)
                 lista.append(velocidade_upload)
-                lista.append(datetime.now().strftime('%d-%m-%Y'))
-                lista.append(datetime.now().strftime('%H-%M-%S')) 
-        
-        except:
-                print('deu erro, esperando 2 segundos')
+                # lista.append(internet.qual_caminho())
+            except:
+                #id = uuid.uuid1()
+                # lista.append(id)
+                lista.append(datetime.now().strftime('%d/%m/%Y'))
+                lista.append(datetime.now().strftime('%H:%M:%S'))
+                lista.append(0)
+                lista.append(0)
+                # lista.append(internet.qual_caminho())
+                #print('deu erro, esperando 2 segundos')
                 time.sleep(2)
-
-
         return lista
 
-def qual_caminho():
-      import socket
-      name = socket.gethostname()
-      if name == 'LGNTVIVO71':
-                caminho = "C:\\Users\\fflose\\Lab\\\Pessoal\\python\\csv\\teste_internet\\"
-      else:
-                caminho =  "D:\\Felipe Flose\\csv\\teste_internet\\"     
-      return(caminho)
+    def qual_caminho():
+        import socket
+        name = socket.gethostname()
+        if name == 'LGNTVIVO71':
+            caminho = "C:\\Users\\fflose\\Lab\\\Pessoal\\python\\csv\\teste_internet\\"
+        else:
+            caminho = "D:\\Felipe Flose\\csv\\teste_internet\\"
+
+        return(caminho)
+
+    def main():
+        x = []
+        x.append(tuple(internet.cria_item()))
+        sql_insert = 'insert into teste ' \
+                     '(dt_teste' \
+                     ',hora_teste' \
+                     ',down' \
+                     ',up) values'
+        for i in x:
+            Sql.cria_tabelas()
+            print(sql_insert + str(i))
+            con = Sql.conexao()
+            cur = Sql.cursor()
+            cur.execute(sql_insert + str(i))
+            con.commit()
+            con.close()
 
 
-def cria_arquivo():
-       
-        dt = datetime.now().strftime('%d-%m-%Y')
-        hr = datetime.now().strftime('%H-%M-%S')     
-        c = qual_caminho()
-        caminho = c+"teste_internet-"+str(dt)+"-"+str(hr)+".csv"
-        cab = ['idExec','dt_teste','hora_teste','hora_fechada','down','chegou','up','dtProcesso','hrProcesso','caminho']
-        f = open(caminho,'w',encoding= 'utf-8',newline='')
-        w = csv.DictWriter(f,delimiter=',', fieldnames=cab)
-        w.writeheader()
-        f = open(caminho,'a',encoding= 'utf-8',newline='')
-        w = csv.writer(f,delimiter=',')
-        lista_grava = cria_lista()
-        lista_grava.append(caminho)
-        w.writerow(lista_grava)
-        
-        #w.writerow(cab)
-        #f.close()
-        #f = open(caminho,'w',newline='',encoding= 'utf-8')
-        #w = csv.writer(f,delimiter=',')        
-        #lista_grava = cria_lista()
-        #lista_grava.append(caminho)
-        #w.writerow(lista_grava)
-        
-        
+class Sql():
+    def conexao():
+        caminhoBD = '\\\\desktop-73e3j72\\bd\\'
+        nomeTable = 'internet.db'
+        con = sqlite3.connect(caminhoBD+nomeTable)
+        return con
 
-def qtd_arquivos():
-        path = qual_caminho()
-        ListaArquivos = os.listdir(path)
-        return len(ListaArquivos)
+    def cria_tabelas():
+        sql_create_teste = 'CREATE TABLE IF NOT EXISTS teste '\
+            '(idExec INTEGER primary key AUTOINCREMENT, '\
+            'dt_teste date, '\
+            'hora_teste time, '\
+            'down int, '\
+            'up int) '
+        # os.remove(caminhoBD+nomeTable) if os.path.exists(caminhoBD+nomeTable) else
+        Sql.exec(sql_create_teste)
 
-def lista_arquivos():
-        
-        path = qual_caminho()
-        ListaArquivos = os.listdir(path)
-        return list(ListaArquivos)
+    def cursor():
+        cur = Sql.conexao().cursor()
+        return cur
 
-def deleta_vazio():
-        for x in lista_arquivos():
-                path = qual_caminho()
-                arq1 = open(path+x)
-                arq1Lendo = csv.reader(arq1,delimiter=',',quoting=csv.QUOTE_NONE)
-                arqLista = list(arq1Lendo)
-                if len(arqLista) == 1:
-                    arq1.close()
-                    os.remove(arq1)        
-                return print('arquivo '+ x +'  deletado!')
-
-def cria_versao_final():
-        path = qual_caminho()
-        c1 = 0
-        caminho = "C:\\Users\\fflose\\Lab\\csv\\annual-enterprise-survey_python.csv"
-        f = open(caminho,'w',newline='',encoding= 'utf-8')
-        ListaArquivos = os.listdir(path)
-        for c in ListaArquivos:
-                nomeArquivo = ListaArquivos[c1]
-                caminho1 = path+nomeArquivo
-                arq1 = open(caminho1)
-                arq1Lendo = csv.reader(arq1,delimiter=';',quoting=csv.QUOTE_NONE)   
-                arqLista = list(arq1Lendo)
-                w = csv.writer(f)
-                w.writerow(arqLista)
-                c1 = c1+1
-                os.remove(caminho1)
+    def exec(x):
+        cur = Sql.conexao().cursor().execute(x)
+        return cur
 
 
-
-#\\desktop-73E3J72\teste_internet\
-
-while 1 == 1 :
-   # deleta_vazio()
-    qual_caminho()
-    cria_arquivo()
-    print(qtd_arquivos())
-    time.sleep(1)
-
-
+if __name__ == "__main__":
+    internet.main()
